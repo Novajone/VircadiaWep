30a2a0b46e9c5f8f0ddb83069451f577ed3c5b42
Novajone <60079867+Novajone@users.noreply.github.com>
2020-10-29 18:42:04 +0000

    Update snipergun.js

--- snipergun.js
+++ snipergun.js
@@ -299,7 +299,7 @@ function exponentialSmoothing(target, current) {
             if (headIndex) {
                 offset = offsetMultiplier* MyAvatar.getAbsoluteJointTranslationInObjectFrame(headIndex).y;
             }
-            result.leftHandPosition = Vec3.multiply(offset, {x: 5.0, y: 1.0, z: 0.0});
+            result.leftHandPosition = Vec3.multiply(offset, {x: 0.25, y: 1.0, z: 0.0});
             var yPosition = exponentialSmoothing(newLeftHandPositionAvatarFrame.y, previousLeftYPosition);
             result.leftHandPosition.y = yPosition;
             previousLeftYPosition = yPosition;
